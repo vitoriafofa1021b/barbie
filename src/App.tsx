57ee9d3bb6747d672043dddb8699b4145e800432
vitoriafofa1021b <vitoria.santos9@estudante.ifms.edu.br>
2023-08-31 09:19:22 -0400

    commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import Header from './componentes/header/Header'
+import Main from './componentes/main/Main'
 import Footer from './componentes/footer/Footer'
 function App() {
   return (
     <>
       <Header/>
+      <Main/>
       <Footer/>
     </>
   )
---
+++ src/componentes/main/Main.tsx
@@ -0,0 +1,10 @@
+import Filme from "../filme/Filme";
+
+export default function Main(){
+    return(
+        <main>
+            <Filme/>
+            <Filme/>
+        </main>
+    )
+}
