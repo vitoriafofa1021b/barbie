409264545f1f0e1c1e154b831f9500f08485b9b4
vitoriafofa1021b <vitoria.santos9@estudante.ifms.edu.br>
2023-08-31 10:17:13 -0400

    atualizado

--- src/componentes/filme/Filme.tsx
+++ src/componentes/filme/Filme.tsx
@@ -1,7 +1,22 @@
-export default function Filme(){
+import './Filme.css'
+
+type Filmeprops={
+    titulo:string,
+    sinopse:string,
+    imagem:string
+
+}
+
+export default function Filme(props:Filmeprops){
   return(
     <div className="filme_content">
-        Filme
+        <img src={props.imagem} alt=""/>
+        <div className="texto_foto">
+            <h1>{props.titulo}</h1>
+            <p className="sinopse">
+            {props.sinopse}
+            </p>
+        </div>
     </div>
   )
 }
--- src/componentes/main/Main.tsx
+++ src/componentes/main/Main.tsx
@@ -1,10 +1,21 @@
 import Filme from "../filme/Filme";
-
+import './Main.css'
 export default function Main(){
     return(
-        <main>
-            <Filme/>
-            <Filme/>
+        <main className="content-main">
+            <Filme titulo="Barbie" sinopse="Depois de ser expulsa da Barbieland
+             por ser uma boneca de aparência menos do que perfeita, 
+             Barbie parte para o mundo humano em busca da verdadeira felicidade."
+             imagem="/barbie.png" />
+
+
+            <Filme titulo="Barbie" sinopse="Depois de ser expulsa da Barbieland
+             por ser uma boneca de aparência menos do que perfeita, 
+             Barbie parte para o mundo humano em busca da verdadeira felicidade."
+             imagem="/pop.star.jpg" />
+             
+
+           
         </main>
     )
 }
